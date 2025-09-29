86fe49d8f5a33573bfe8e139914a835f32f6d2a6
Madzy27-Ys <yanis.switaj27@gmail.com>
2025-09-29 17:40:01 +0200

    fin

--- lib/gossip.rb
+++ lib/gossip.rb
@@ -9,15 +9,38 @@ class Gossip
   end
 
   def save
-    dir = File.join(__dir__, "db")
-    Dir.mkdir(dir) unless Dir.exist?(dir)
-    filepath = File.join(__dir__, "db", "gossip.csv")
-    puts "Chemin de ton potin: #{filepath}"
+  dir = File.join(__dir__, "db")
+  Dir.mkdir(dir) unless Dir.exist?(dir)
+  filepath = File.join(__dir__, "db", "gossip.csv")
+  
+  puts "="*20
+  puts "Sauvegarde en cours.."
+  puts "Chemin : #{File.expand_path(filepath)}"
+  puts "Auteur : #{@author}"
+  puts "Contenu : #{@content}"
+  puts "="*20
+  
+  CSV.open(filepath, "ab") do |csv|
+    csv << [@author, @content]
+end
+end
 
-    CSV.open(filepath, "ab") do |csv|
-      csv << [author, content]
+ def self.all
+    all_gossips = []
+    filepath = File.join(__dir__, "db", "gossip.csv")
+
+    # On lit chaque ligne du CSV
+    CSV.read(filepath).each do |csv_line|
+      all_gossips << Gossip.new(csv_line[0], csv_line[1])
     end
+
+    return all_gossips
+  end
+
+  def self.find(id)
+    all_gossips = self.all
+    all_gossips[id.to_i]
   end
 end
 
-Gossip.new("José", "Josiane est nulle").save
+# Gossip.new("José", "Josiane est nulle").save
